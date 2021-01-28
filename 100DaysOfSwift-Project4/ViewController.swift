e915003ea052724c4bc26fe6dc84dec15877b133
João Gabriel Dourado Cervo <joaogabrieldouradocervo@gmail.com>
2021-01-28 08:59:32 -0300

    ADD progressView e KVO

--- 100DaysOfSwift-Project4/ViewController.swift
+++ 100DaysOfSwift-Project4/ViewController.swift
@@ -10,6 +10,7 @@ import WebKit
 
 class ViewController: UIViewController, WKNavigationDelegate {
     var webView: WKWebView!
+    var progressView: UIProgressView!
     
     override func loadView() {
         // Muda a view principal para carregar o webView ao invés do storyboard
@@ -23,11 +24,16 @@ class ViewController: UIViewController, WKNavigationDelegate {
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
         
+        progressView = UIProgressView(progressViewStyle: .default) // tem tambem o modo .bar, ele nao mostra uma linha vazia, mas nesse caso o .default fica melhor
+        progressView.sizeToFit() // Enquadra conteudo para ver inteiro
+        
+        let progressButton = UIBarButtonItem(customView: progressView) // Cria um BarButton com uma custom view
+        
         // Itens para toolbar
         let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
         let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
         
-        toolbarItems = [spacer, refresh] // define os itens
+        toolbarItems = [progressButton, spacer, refresh] // define os itens
         navigationController?.isToolbarHidden = false // mostra a toolbar
         // Toolbar -> barra embaixo
         
@@ -35,6 +41,21 @@ class ViewController: UIViewController, WKNavigationDelegate {
         
         webView.load(URLRequest(url: url)) // Necessario fazer o request, se nao webView nao carrega a pag
         webView.allowsBackForwardNavigationGestures = true // Passar dedo pra ir/votar pag, sempre bom ter
+        
+        /**
+         KVO -> Key-value-observing: visa a view quando o valor X da propriedade Y mudar
+         
+         #Warning: in more complex applications, all calls to addObserver() should be matched with a call to removeObserver() when you're finished observing – for example, when you're done with the view controller.
+         */
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+        /**
+        o options .new manda junto o novo valor, e nao apenas diz que mudou, .old o antigo e etc...
+        self no observer pois é esta classe que vai observar as mudanças
+         
+        keyPath funciona como o #selector... Faz com que o compilador verifique que o valor existe de fato ali
+         
+        o context provem um valor unico, que é mandado de volta quando se tem a notificação que o valor mudou, isso faz com que consiga se verificar o contexto e ter certeza que foi o observer x que foi chamado
+        */
     }
     
     @objc func openTapped() {
@@ -61,7 +82,12 @@ class ViewController: UIViewController, WKNavigationDelegate {
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         title = webView.title
     }
-
-
+    
+    // Necessario implementar devido ai KVO
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+        }
+    }
 }
 
