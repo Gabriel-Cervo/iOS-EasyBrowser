c6b2e29b0b755582ef4163a3bb0f83ebaf2c06bf
João Gabriel Dourado Cervo <joaogabrieldouradocervo@gmail.com>
2021-01-28 08:40:41 -0300

    ADD mudanca de titulo e paginas

--- 100DaysOfSwift-Project4/ViewController.swift
+++ 100DaysOfSwift-Project4/ViewController.swift
@@ -21,10 +21,37 @@ class ViewController: UIViewController, WKNavigationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let url = URL(string: "https://github.com")!
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
+        let url = URL(string: "https://google.com")!
         
         webView.load(URLRequest(url: url)) // Necessario fazer o request, se nao webView nao carrega a pag
         webView.allowsBackForwardNavigationGestures = true // Passar dedo pra ir/votar pag, sempre bom ter
+    }
+    
+    @objc func openTapped() {
+        let alertController = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+        
+        alertController.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        alertController.addAction(UIAlertAction(title: "github.com", style: .default, handler: openPage))
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+
+        
+        alertController.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+        
+        present(alertController, animated: true)
+    }
+    
+    func openPage(action: UIAlertAction) {
+        let url = URL(string: "http://\(action.title!)")!
+        
+        webView.load(URLRequest(url: url)) // carrega na webView
+    }
+    
+    // Metodo que faz algo quando pag for carregada
+    // necessario webView delegate ser para esta classe
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
     }
 
 
